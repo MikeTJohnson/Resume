e280e5d2daa2c1787def63f585b29add4e6e9f7d
MikeTJohnson <mtjzdd@gmail.com>
2024-01-19 13:22:30 -0700

    adding zddController

---
+++ ZDDController/ZDDController/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ZDDController;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new NavigationPage(new MainPage());
+	}
+}
+
---
+++ ZDDController/ZDDController/Views/NewInventory.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace ZDDController.Views;
+
+public partial class NewInventory : ContentPage
+{
+	public NewInventory()
+	{
+		InitializeComponent();
+	}
+
+    private void onInventoryButtonClicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new AddInventory());
+    }
+
+    private void onHomeButtonClicked(object sender, EventArgs e)
+    {
+        Navigation.PopToRootAsync();
+    }
+
+    private void onTTFButtonClicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new AddTTF());
+    }
+}
